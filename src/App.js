487bea7084a0fccfef7e810093c436ce6c00b566
Mahesh Kasbe <maheshkasbe010@gmail.com>
2022-05-06 20:38:28 +0530

    Commit One

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css"
 
 function App() {
+  const [username, setUsername] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="username" value={username} onChange={(e) => {
+        setUsername(e.target.value)
+      }}/>
+      <input type="password" placeholder="password" value="password" />
+      <button > Login </button>
+      <span data-testid="error" > something went wrong </span>
+
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,42 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('username input show be rendered', () =>{
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const userinput = screen.getByPlaceholderText(/username/i);
+  expect(userinput).toBeInTheDocument();
+})
+
+test('pass input show be rendered', () =>{
+  render(<App />);
+  const passinput = screen.getByPlaceholderText(/password/i);
+  expect(passinput).toBeInTheDocument();
+})
+
+test('login input show be rendered', () =>{
+  render(<App />);
+  const logininput = screen.getByRole("button");
+  expect(logininput).toBeInTheDocument();
+})
+
+test('login input should be disabled', () =>{
+  render(<App />);
+  const logininput = screen.getByRole("button");
+  expect(logininput).not.toBeDisabled();
+})
+
+test('err msg should be inivisible', () =>{
+  render(<App />);
+  const err = screen.getByTestId("error");
+  expect(err).toBeVisible();
+})
+
+test('username input should be testval', () =>{
+  render(<App />);
+  const userinput = screen.getByPlaceholderText(/username/i);
+  const testvalue = "test";
+
+  fireEvent.change(userinput, { target: { value: testvalue } });
+  expect(userinput.value).toBe(testvalue);  
+})
+
